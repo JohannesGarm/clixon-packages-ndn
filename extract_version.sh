b5bc2338b9546f02ae2080e81cd1144b8e34d8de
Kristofer Hallin <kristofer@sunet.se>
2024-01-04 12:01:14 +0100

    Update extract_version.sh

--- extract_version.sh
+++ extract_version.sh
@@ -16,24 +16,6 @@ fi
 repo="${repo%/}"
 cd "$repo"
 
-
-if [ $repo = "cligen" ]; then
-    major=$(grep -oP '(?<=CLIGEN_VERSION_MAJOR=")\d+' configure.ac)
-    minor=$(grep -oP '(?<=CLIGEN_VERSION_MINOR=")\d+' configure.ac)
-    patch=$(grep -oP '(?<=CLIGEN_VERSION_PATCH=")\d+' configure.ac)
-    version=$major.$minor.$patch
-elif [ $repo = "clixon" ]; then
-    major=$(grep -oP '(?<=CLIXON_VERSION_MAJOR=")\d+' configure.ac)
-    minor=$(grep -oP '(?<=CLIXON_VERSION_MINOR=")\d+' configure.ac)
-    patch=$(grep -oP '(?<=CLIXON_VERSION_PATCH=")\d+' configure.ac)
-    version=$major.$minor.$patch    
-elif [ $repo = "clixon-controller" ]; then
-    major=$(grep -oP '(?<=CONTROLLER_VERSION_MAJOR=")\d+' configure.ac)
-    minor=$(grep -oP '(?<=CONTROLLER_VERSION_MINOR=")\d+' configure.ac)
-    patch=$(grep -oP '(?<=CONTROLLER_VERSION_PATCH=")\d+' configure.ac)
-    version=$major.$minor.$patch
-elif [ $repo = "clixon-pyapi" ]; then
-    version=$(grep -oP '(?<=version=")\d+.\d+.\d+' setup.py)    
-fi
+version=$(git describe --tags --abbrev=0)
 
 echo $version
